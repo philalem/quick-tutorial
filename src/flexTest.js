ff32fd395b1d7edf1497b56fdae7b48c93b261c3
Phillip LeMaster <philalem21@gmail.com>
2020-04-25 16:44:43 -0400

    added navbar section to container

--- src/flexTest.js
+++ src/flexTest.js
@@ -4,8 +4,13 @@ import "./test.css";
 export default function flexTest() {
   return (
     <div className="container">
-      <div className="sidebar">Hellooooooooo</div>
-      <div className="feed">hiiiii</div>
+      <div className="row">
+        <div className="navbar">THE NAVBAR</div>
+      </div>
+      <div className="row">
+        <div className="sidebar">Hellooooooooo</div>
+        <div className="feed">hiiiii</div>
+      </div>
     </div>
   );
 }
